b27d0deafd31ce50e973fa468f459bf53e695235
sa2ed <sa2edabusham87@gmail.com>
2022-08-16 00:08:00 +0300

    under dev

--- app/src/main/java/com/raantech/awfrlak/store/ui/base/bindingadapters/CountryCodePickerBindingAdapter.kt
+++ app/src/main/java/com/raantech/awfrlak/store/ui/base/bindingadapters/CountryCodePickerBindingAdapter.kt
@@ -1,10 +1,17 @@
 package com.raantech.awfrlak.store.ui.base.bindingadapters
 
+import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import com.raantech.awfrlak.R
+import com.raantech.awfrlak.store.utils.extensions.gone
 import com.rilixtech.widget.countrycodepicker.CountryCodePicker
 
 @BindingAdapter("ccp_selected_country_code_iso2")
 fun CountryCodePicker?.setSelectedCountryCodeUsingISO2Name(iso2Name:String?){
     if(iso2Name.isNullOrEmpty()) return
     this?.setCountryForNameCode(iso2Name)
+}
+@BindingAdapter("ccp_hide_arrow")
+fun CountryCodePicker?.hideArrow(hide:Boolean){
+    this?.findViewById<ImageView>(R.id.arrow_imv)?.gone()
 }
